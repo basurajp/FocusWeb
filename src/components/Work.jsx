43dd4341c2ccff518b2bb1a9b052b291ca4817b2
Basuraj <basurajp135@gmail.com>
2024-01-31 22:27:56 +0530

    work image part completed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React from "react";
 
 const Work = () => {
-  return (
-    <div className='w-full  '>
-        <div className="max-w-screen-xl mx-auto">
-            <h1 className='text-[36vw] leading-[32vw] font-medium text-center select-none'>work</h1>
-        </div>
-      
-    </div>
-  )
-}
+  const images = [
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef09178195ce0073e38f3_Refokus%20Tools-1.png",
+      top: "50%",
+      left: "50%",
+      isActive: true,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0accfe1b3e66bc55462_Refokus%20Tools.png",
+      top: "56%",
+      left: "44%",
+      isActive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0acbc45cb2f4fc5c6b2_Yahoo.png",
+      top: "45%",
+      left: "56%",
+      isActive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef092455ce2cf591e52d1_Rainfall.png",
+      top: "60%",
+      left: "53%",
+      isActive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0ac7e7179d210dc41f0_Summon.png",
+      top: "43%",
+      left: "50%",
+      isActive: false,
+    },
+    {
+      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/634ef0af108a465002975acd_Showcase%20Websites%20(1).png",
+      top: "60%%",
+      left: "55%",
+      isActive: false,
+    },
+  ];
 
-export default Work
+  return (
+    <div className="w-full  ">
+      <div className="max-w-screen-xl mx-auto relative">
+        <h1 className="text-[36vw] leading-[32vw] font-medium text-center select-none">
+          work
+        </h1>
+        <div className="w-full h-full  absolute top-0">
+          {images.map((elem, index) =>elem.isActive && (
+            <img style={{
+                top:elem.top,
+                left:elem.left
+
+            }} className="w-60 rounded-lg absolute -translate-x-[50%] -translate-y-[50%]" src={elem.url} alt="this image " />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
